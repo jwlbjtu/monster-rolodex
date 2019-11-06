2bcfada7fd23de7a1846f095f538c3e184227e53
jwlbjtu <jwlbjtu@gmail.com>
2019-11-05 22:12:50 -0500

    Updates

---
+++ precache-manifest.e43ff943079ee4f0be1867a61fc770ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffea495a03f9f0cbafcc6ea79814359",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c1b80f1db1290e3448b3",
+    "url": "/monster-rolodex/static/css/main.dfb27c73.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monster-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "c1b80f1db1290e3448b3",
+    "url": "/monster-rolodex/static/js/main.553463ef.chunk.js"
+  },
+  {
+    "revision": "e5540c1030566d31d8b0",
+    "url": "/monster-rolodex/static/js/runtime-main.bd5b8595.js"
+  }
+]);
